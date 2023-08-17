ef40bb16bf13af86ec1a264bc8483431e0ed8a15
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-08-17 16:09:53 +0500

    Update auth_screen.dart

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quick_chat/widgets/auth/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -11,11 +13,28 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
 
   void _submitAuthForm(
-    String email,
-    String password,
+    String useremail,
+    String userpassword,
     String username,
     bool isLogin,
-  ) {}
+  ) async {
+    UserCredential authResult;
+    try {
+    if (isLogin) {
+      authResult = await FirebaseAuth.instance.signInWithEmailAndPassword(email: useremail, password: userpassword);
+    } else {
+      authResult = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: useremail, password: userpassword);
+    }
+    } on PlatformException catch (err) {
+      String msg = 'please enter right credentials!';
+      if (err.message != null) {
+        msg = err.message!;
+      }
+      SnackBar(content: Text(msg));
+    } catch (err) {
+      print(err);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
