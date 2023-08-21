6bf37e9942e73f18d84310ef0c014503bd840b20
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-08-21 12:57:21 +0500

    Update message_bubble.dart

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -8,12 +8,20 @@ class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
             color:
                 isMe ? Colors.grey[300] : Theme.of(context).colorScheme.primary,
-            borderRadius: BorderRadius.circular(12),
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(12),
+              topRight: const Radius.circular(12),
+              bottomLeft:
+                  isMe ? const Radius.circular(12) : const Radius.circular(0),
+              bottomRight:
+                  isMe ? const Radius.circular(0) : const Radius.circular(12),
+            ),
           ),
           width: 140,
           padding: const EdgeInsets.symmetric(
